0bd2ac40f579a4ebacae764166aaf7c2d391dc30
Artem Musii <artem.musii@gmail.com>
2026-03-22 22:26:37 +0100

    feat: adapt event publisher for connection manager

--- src/infrastructure/rabbitmq/event-publisher.js
+++ src/infrastructure/rabbitmq/event-publisher.js
@@ -3,22 +3,33 @@ const EXCHANGE = 'email.events'
 const noop = () => {}
 const noopLog = { debug: noop, info: noop, warn: noop, error: noop }
 
-const RabbitMQPublisher = (channel, { log = noopLog } = {}) => {
+const RabbitMQPublisher = (connectionManager, { exchange = EXCHANGE, type = 'topic', log = noopLog } = {}) => {
   let ready = false
 
   const init = async () => {
-    await channel.assertExchange(EXCHANGE, 'topic', { durable: true })
+    const channel = connectionManager.getChannel()
+    if (!channel) throw new Error('RabbitMQ channel not available')
+    await channel.assertExchange(exchange, type, { durable: true })
     ready = true
-    log.info('exchange initialized', { exchange: EXCHANGE })
+    log.info('exchange initialized', { exchange })
   }
 
   const publish = async (event) => {
+    const channel = connectionManager.getChannel()
+    if (!channel) {
+      log.warn('rabbitmq not connected, event dropped', { type: event.type })
+      return
+    }
     if (!ready) await init()
-    channel.publish(EXCHANGE, event.type, Buffer.from(JSON.stringify(event)), { persistent: true })
+    channel.publish(exchange, event.type, Buffer.from(JSON.stringify(event)), { persistent: true })
     log.debug('event published', { type: event.type })
   }
 
-  return { publish, init }
+  const reset = () => {
+    ready = false
+  }
+
+  return { publish, init, reset }
 }
 
 export { RabbitMQPublisher }
